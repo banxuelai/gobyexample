8c562763ee5644611e7918d13d9d6ed3445a9c67
banxuelai160714 <banxuelai>
2017-12-15 11:46:03 +0800

    values

--- hello.go
+++ hello.go
@@ -7,4 +7,13 @@ import (
 func main() {
 	fmt.Printf("Hello_World\n")
 	fmt.Println("Hello_World2") //输出自带空格
+
+	//值 字符串 整形  浮点 布尔 逻辑运算符
+	fmt.Println("go" + " lang")
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("10.0/3.0=", 10.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
